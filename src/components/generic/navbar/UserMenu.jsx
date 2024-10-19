101f2ad766b0ee7ff85477004646f9714ee0f32d
Devisson Vasquez <devissonvasquez99@gmail.com>
2024-10-18 23:14:44 -0500

    add change password

--- src/components/generic/navbar/UserMenu.jsx
+++ src/components/generic/navbar/UserMenu.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import { IconButton, Menu, MenuItem, Avatar, Tooltip, Typography, ListItemIcon } from '@mui/material';
+import { Edit as EditIcon, Logout as LogoutIcon, Lock as LockIcon } from '@mui/icons-material'; 
 import { useTranslation } from 'react-i18next';
 import useAuthStore from '../../../stores/auth/useAuthStore'; 
 import UserEditModal from '../../users-components/UserEditModal';
-import { useUsers } from '../../../hooks/users/useUsers'; 
-import { Edit as EditIcon, Logout as LogoutIcon } from '@mui/icons-material'; 
+import UserChangePasswordModal from '../../users-components/UserChangePasswordModal'; 
+import { useUsers } from '../../../hooks/users/useUsers';
 
 const UserMenu = ({ handleLogout }) => {
   const [anchorElUser, setAnchorElUser] = useState(null);
-  const [openEditModal, setOpenEditModal] = useState(false); 
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const [openPasswordModal, setOpenPasswordModal] = useState(false);
   const { t } = useTranslation();
-  const { token, user } = useAuthStore(); 
-  const { handleUpdateUser } = useUsers(); 
+  const { token, user } = useAuthStore();
+  const { handleUpdateUser } = useUsers();
 
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
@@ -21,13 +23,18 @@ const UserMenu = ({ handleLogout }) => {
     setAnchorElUser(null);
   };
 
-  const handleLogin = () => {
-    window.location.href = "/login"; 
+  const handleEditProfile = () => {
+    setOpenEditModal(true);
+    setAnchorElUser(null);
   };
 
-  const handleEditProfile = () => {
-    setOpenEditModal(true); 
-    setAnchorElUser(null); 
+  const handleChangePassword = () => {
+    setOpenPasswordModal(true);
+    setAnchorElUser(null);
+  };
+
+  const handleLogin = () => {
+    window.location.href = "/login";
   };
 
   return (
@@ -48,19 +55,26 @@ const UserMenu = ({ handleLogout }) => {
         onClose={handleCloseUserMenu}
       >
         {token ? (
-          [
+          <div>
             <MenuItem key="user-infos" disabled>
               <Typography variant="h5" textAlign="center">
                 {`${user?.firstName} ${user?.lastName}`}  
               </Typography>
-            </MenuItem>,
+            </MenuItem>
 
             <MenuItem key="edit-profile" onClick={handleEditProfile}>
               <ListItemIcon>
-                <EditIcon fontSize="small" />
+                <EditIcon fontSize="small" /> 
               </ListItemIcon>
               {t('edit_profile')}
-            </MenuItem>,
+            </MenuItem>
+
+            <MenuItem key="change-password" onClick={handleChangePassword}>
+              <ListItemIcon>
+                <LockIcon fontSize="small" /> 
+              </ListItemIcon>
+              {t('change_password')}
+            </MenuItem>
             
             <MenuItem key="logout" onClick={handleLogout}>
               <ListItemIcon>
@@ -68,18 +82,15 @@ const UserMenu = ({ handleLogout }) => {
               </ListItemIcon>
               {t('logout')}
             </MenuItem>
-          ]
+          </div>
         ) : (
           <MenuItem onClick={handleLogin}>{t('login')}</MenuItem>
         )}
       </Menu>
 
-      <UserEditModal 
-        open={openEditModal} 
-        onClose={() => setOpenEditModal(false)} 
-        currentUser={user} 
-        onEditUser={handleUpdateUser} 
-      />
+      {/* Modals */}
+      <UserEditModal open={openEditModal} onClose={() => setOpenEditModal(false)} currentUser={user} onEditUser={handleUpdateUser} />
+      <UserChangePasswordModal open={openPasswordModal} onClose={() => setOpenPasswordModal(false)} userId={user?.id} />
     </>
   );
 };
