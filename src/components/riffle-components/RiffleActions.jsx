5958572d55bed497fe384fa22103b5f6d9fdfcc2
Devisson Vasquez <devissonvasquez99@gmail.com>
2024-10-18 22:28:37 -0500

    change access for role

--- src/components/riffle-components/RiffleActions.jsx
+++ src/components/riffle-components/RiffleActions.jsx
@@ -2,19 +2,28 @@ import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import useAuthStore from '../../stores/auth/useAuthStore';
 
-const RiffleActions = ({ riffleId, onEdit, onDelete, onViewTickets }) => (
-  <>
-    <IconButton color="primary" onClick={() => onEdit(riffleId)}>
-      <EditIcon />
-    </IconButton>
-    <IconButton color="error" onClick={() => onDelete(riffleId)}>
-      <DeleteIcon />
-    </IconButton>
-    <IconButton color="info" onClick={() => onViewTickets(riffleId)}>
-      <VisibilityIcon />
-    </IconButton>
-  </>
-);
+const RiffleActions = ({ riffleId, onEdit, onDelete, onViewTickets }) => {
+  const { user } = useAuthStore();
+
+  return (
+    <>
+      {user?.role === 'admin' && (
+        <>
+          <IconButton color="primary" onClick={() => onEdit(riffleId)}>
+            <EditIcon />
+          </IconButton>
+          <IconButton color="error" onClick={() => onDelete(riffleId)}>
+            <DeleteIcon />
+          </IconButton>
+        </>
+      )}
+      <IconButton color="info" onClick={() => onViewTickets(riffleId)}>
+        <VisibilityIcon />
+      </IconButton>
+    </>
+  );
+};
 
 export default RiffleActions;
