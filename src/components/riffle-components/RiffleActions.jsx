9b131d31f0dc8ae01e9b8cc08a1b45cf6042970f
Devisson Vasquez <devissonvasquez99@gmail.com>
2024-10-20 00:28:54 -0500

    tooltips and messages

--- src/components/riffle-components/RiffleActions.jsx
+++ src/components/riffle-components/RiffleActions.jsx
@@ -1,28 +1,38 @@
-import { IconButton } from '@mui/material';
+import { IconButton, Tooltip } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import useAuthStore from '../../stores/auth/useAuthStore';
+import { useTranslation } from 'react-i18next';
 import { ROLE_ADMIN } from '../../utils/generic/constants';
 
 const RiffleActions = ({ riffleId, onEdit, onDelete, onViewTickets }) => {
   const { user } = useAuthStore();
+  const { t } = useTranslation();
 
   return (
     <>
       {user?.role === ROLE_ADMIN && (
         <>
-          <IconButton color="primary" onClick={() => onEdit(riffleId)}>
-            <EditIcon />
-          </IconButton>
-          <IconButton color="error" onClick={() => onDelete(riffleId)}>
-            <DeleteIcon />
-          </IconButton>
+          <Tooltip title={t('edit_riffle')}>
+            <IconButton color="primary" onClick={() => onEdit(riffleId)}>
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title={t('delete_riffle')}>
+            <IconButton color="error" onClick={() => onDelete(riffleId)}>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
         </>
       )}
-      <IconButton color="info" onClick={() => onViewTickets(riffleId)}>
-        <VisibilityIcon />
-      </IconButton>
+      
+      <Tooltip title={t('view_tickets')}>
+        <IconButton color="info" onClick={() => onViewTickets(riffleId)}>
+          <VisibilityIcon />
+        </IconButton>
+      </Tooltip>
     </>
   );
 };
