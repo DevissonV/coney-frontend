659553d972d751a64ff98dba3ff695a560b49d8c
Devisson Vasquez <devissonvasquez99@gmail.com>
2025-04-05 15:26:52 -0500

    feat: add button change languaje

---
+++ src/components/generic/navbar/LanguageToggleButton.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+  ListItemIcon,
+  Typography,
+} from '@mui/material';
+import LanguageIcon from '@mui/icons-material/Language';
+import CheckIcon from '@mui/icons-material/Check';
+import { useTranslation } from 'react-i18next';
+
+const languages = [
+  { code: 'en', label: 'English' },
+  { code: 'es', label: 'Español' },
+];
+
+const LanguageToggleButton = () => {
+  const { i18n, t } = useTranslation();
+  const currentLang = i18n.language;
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleSelectLanguage = (code) => {
+    i18n.changeLanguage(code);
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Tooltip title={t('change_language')}>
+        <IconButton color="inherit" onClick={handleOpenMenu}>
+          <LanguageIcon />
+        </IconButton>
+      </Tooltip>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={() => setAnchorEl(null)}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      >
+        {languages.map((lang) => (
+          <MenuItem
+            key={lang.code}
+            selected={lang.code === currentLang}
+            onClick={() => handleSelectLanguage(lang.code)}
+          >
+            <ListItemIcon>
+              {lang.code === currentLang ? <CheckIcon fontSize="small" /> : null}
+            </ListItemIcon>
+            <Typography>{lang.label}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+};
+
+export default LanguageToggleButton;
